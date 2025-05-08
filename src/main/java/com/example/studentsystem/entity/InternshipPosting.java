1ddbf0f15480e2113766362b916c4b3cdd0ca89b
dilarakalkan <dilarakalkanxx@gmail.com>
2025-05-08 21:13:53 +0300

    EN SON GÜNCELLEME YAPILDI CURSOR ÖNCESİ

--- src/main/java/com/example/studentsystem/entity/InternshipPosting.java
+++ src/main/java/com/example/studentsystem/entity/InternshipPosting.java
@@ -1,27 +1,31 @@
 package com.example.studentsystem.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 //şirketin açtığı ilan
 
 @Getter
 @Setter
+
 @Entity
 public class InternshipPosting {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    private String company;
-    private String city;
-    private String field;
-    private LocalDateTime applicationDate;
+        private Long id;
+        private String title;
+        private String description;
+        public LocalDate startDate;
+        private LocalDate endDate;
 
-
+        @ManyToOne
+        private Company company;
+        {
+    }
 }
+
+
